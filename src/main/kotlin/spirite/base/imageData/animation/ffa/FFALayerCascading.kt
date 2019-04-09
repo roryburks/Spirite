0d56160dcd0649972c5888af61685ab96cb47f69
Rory Burks <roryaburks@gmail.com>
2019-04-09 12:33:49 -0400

    Working on FFA Cascading Layer

--- src/main/kotlin/spirite/base/imageData/animation/ffa/FFALayerCascading.kt
+++ src/main/kotlin/spirite/base/imageData/animation/ffa/FFALayerCascading.kt
@@ -1,9 +1,11 @@
 package spirite.base.imageData.animation.ffa
 
-import spirite.base.graphics.rendering.TransformedHandle
+import rb.extendo.extensions.toHashMap
+import rb.vectrix.mathUtil.MathUtil
 import spirite.base.imageData.animation.ffa.FixedFrameAnimation.FFAUpdateContract
 import spirite.base.imageData.groupTree.GroupTree.GroupNode
 import spirite.base.imageData.groupTree.GroupTree.LayerNode
+import kotlin.math.min
 
 class FFALayerCascading(
         override val anim: FixedFrameAnimation,
@@ -11,47 +13,143 @@ class FFALayerCascading(
         lexicon: String = "")
     :IFFALayer, IFFALayerLinked
 {
+    var lexicon: String? = null
+        set(value) {
+            field = value
+            update()
+        }
+
+    var sublayerInfo = mutableMapOf<GroupNode, FFACascadingSublayerInfo>()
+    //var sublayers : List<Pair<Int, FFACascadingSublayerInfo?>> = emptyList()
+
     // region IFFALayer
     override val start: Int get() = 0
-    override val end: Int
-        get() = TODO("not implemented")
-    override var asynchronous: Boolean
-        get() = TODO("not implemented")
-        set(value) {}
-    override val frames: List<IFFAFrame>
-        get() = TODO("not implemented")
-
-    override fun getFrameFromLocalMet(met: Int, loop: Boolean): IFFAFrame? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override var end: Int = 0 ; private set
+    override var asynchronous: Boolean = false
+    override var frames: List<IFFAFrame> = emptyList(); private set
+    override fun getFrameFromLocalMet(met: Int, loop: Boolean): IFFAFrame? =
+            frames.getOrNull(if( loop) MathUtil.cycle(0, end, met) else met)
     // endregion
 
     // region IFFALayerLinked
-    override fun groupLinkUpdated() {
-        groups = groupLink.children.filterIsInstance<GroupNode>()
-    }
-
+    override fun groupLinkUpdated() {update()}
     override fun shouldUpdate(contract: FFAUpdateContract) = contract.ancestors.contains(groupLink)
     // Endregion
 
+    // region exposed functionality
+    // endregion
 
-    var lexicon: String = ""
-        set(value) {
-            field = value
-            anim.triggerFFAChange(this)
+    private data class IndexedSublayer(val start: Int, val info: FFACascadingSublayerInfo?)
+
+    private fun update()
+    {
+        val newGroupNodes = groupLink.children.filterIsInstance<GroupNode>()
+
+        val oldSublayerInfo = sublayerInfo
+        val newSublayerInfo = newGroupNodes
+                .mapIndexedNotNull { index, groupNode ->
+                    val key = 'A' + index
+                    val info = oldSublayerInfo[groupNode]?.copyUpdated(key)
+                            ?: FFACascadingSublayerInfo.FromGroup(groupNode, key)
+                            ?: return@mapIndexedNotNull  null
+                    Pair(groupNode, info)
+                }
+                .toMap()
+        sublayerInfo = newSublayerInfo.toMutableMap()
+
+        val oldLexicalMap = oldSublayerInfo.values.toHashMap({it.lexicalKey}, {it.group})
+        val newLexicalMap = newSublayerInfo.values.toHashMap ({it.lexicalKey}, {it})
+        val newLexicalInvMap = newSublayerInfo.values.toHashMap({it.group}, {it.lexicalKey})
+
+        // Remap lexicon
+        val oldLexicon = lexicon
+        lexicon = oldLexicon?.asSequence()
+                ?.mapNotNull { oldLexicalMap[it]?.run { newLexicalInvMap[this] } }
+                ?.joinToString()
+        val newLexicon = lexicon ?: newLexicalInvMap.values.joinToString()
+
+        var met = 0
+        val sublayers = newLexicon.mapNotNull {
+            val info = newLexicalMap[it]
+            IndexedSublayer(met, info).also { met += info?.primaryLen ?: 1 }
         }
 
-    var groups = listOf<GroupNode>()
+        // Recalc Frames
+        calcFrames(sublayers)
+
+        anim.triggerFFAChange(this)
+    }
+
+    private fun calcFrames( sublayers: List<IndexedSublayer>)
+    {
+        // Recalc Len
+        end = sublayers.asSequence().map { it.start + (it.info?.len ?: 1) }.max() ?: 0
+
+        frames = (0 until end).map { i -> CascadingFrame(
+                i,
+                sublayers
+                        .filter {it.info != null && it.start <= i && (it.start + it.info.primaryLen ) > i }
+                        .map { Pair(i - it.start, it.info!!) }
+                )
+        }
+    }
+
 
     inner class CascadingFrame(
             override val start: Int,
-            val layers : List<LayerNode>
-    )
+            val points: List<Pair<Int, FFACascadingSublayerInfo>>)
         :IFFAFrame
     {
         override val layer: IFFALayer get() = this@FFALayerCascading
         override val length: Int get() = 1
-        override fun getDrawList() = layers.flatMap { it.getDrawList() }
+        override fun getDrawList() = points
+                .mapNotNull { it.second.getLayerFromLocalMet(it.first) }
+                .flatMap { it.getDrawList() }
+    }
+}
+
+
+data class FFACascadingSublayerInfo (
+        val group: GroupNode,
+        val lexicalKey: Char,
+        val layers: List<LayerNode>,
+        val primaryLen: Int,
+        val lexicon: String? = null )
+{
+    val len: Int get() = lexicon?.length ?: layers.count()
+
+    fun copyUpdated(newLexicalKey: Char) : FFACascadingSublayerInfo? {
+        val newLayerNodes = group.children.filterIsInstance<LayerNode>()
+                .asReversed()
+        if(!newLayerNodes.any())
+            return null
+        val plen = min(primaryLen, newLayerNodes.size)
+
+        // TODO: Remap Lexicon
+
+        return this.copy(layers = newLayerNodes, lexicalKey = newLexicalKey, primaryLen =  plen)
     }
 
+    fun getLayerFromLocalMet(met: Int) : LayerNode? = when {
+        met < 0 -> null
+        lexicon == null -> layers.getOrNull(met)
+        else -> when(val lex = lexicon.getOrNull(met)) {
+            null -> null
+            else -> layers.getOrNull(lex - 'A')
+        }
+    }
+
+    companion object {
+        fun FromGroup(group: GroupNode, lexicalKey: Char) : FFACascadingSublayerInfo? {
+            val layerNodes = group.children.filterIsInstance<LayerNode>()
+                    .asReversed()
+            if( !layerNodes.any())
+                return null
+            return FFACascadingSublayerInfo(
+                    group,
+                    lexicalKey,
+                    layerNodes,
+                    layerNodes.count())
+        }
+    }
 }
