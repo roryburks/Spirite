074d390ee008980d71295dc96384b39753e60011
Rory Burks <roryaburks@gmail.com>
2019-12-01 19:23:50 -0500

    Weak Obs Tests, Splititng up WeakBindable, etc

--- src/main/kotlin/rbJvm/owl/WeakObserver.kt
+++ src/main/kotlin/rbJvm/owl/WeakObserver.kt
@@ -2,15 +2,10 @@ package rbJvm.owl
 
 import rb.IContract
 import rb.extendo.dataStructures.SinglySequence
-import rb.extendo.extensions.mapRemoveIfNull
 import rb.owl.IObservable
 import rb.owl.IObserver
-import rb.owl.bindable.Bindable
-import rb.owl.bindable.IBindObserver
 import rb.owl.bindable.IBindable
-import rb.owl.bindable.OnChangeEvent
 import java.lang.ref.WeakReference
-import kotlin.reflect.KProperty
 
 /** A WeakObserver is an Observer that does not have a strong reference to the trigger.  The idea being that the trigger
  * will have strong references to several potentially-large objects.  Someone else should keep a firm rederence to the
@@ -25,11 +20,11 @@ class WeakObserver<T>(trigger: T) : IObserver<T>
 }
 
 /** Note: the contract strongly references the Trigger, so the t is preserved as long as the contract is. */
-fun <T> IObservable<T>.addWeakObserver(t: T, trigger: Boolean = true) : IContract =
+fun <T> IObservable<T>.addWeakObserver(t: T) : IContract =
         WeakObserverContract(this.addObserver(WeakObserver(t)), t)
 
 /** Note: the contract strongly references the Trigger, so the trigger is preserved as long as the contract is. */
-fun <T> IBindable<T>.addWeakObserver(trigger: Boolean = true, t: (new: T, old: T)->Unit) : IContract =
+fun <T> IBindable<T>.addWeakObserver(t: (new: T, old: T)->Unit) : IContract =
         WeakObserverContract(this.addObserver(WeakObserver(t)),t)
 
 private class WeakObserverContract<T>(private val bindContract: IContract, t: T) : IContract {
@@ -40,66 +35,3 @@ private class WeakObserverContract<T>(private val bindContract: IContract, t: T)
     }
 }
 
-class WeakBindable<T>(default: T) : IObservable<OnChangeEvent<T>>
-{
-    val bind = Bindable(default)
-
-    override fun addObserver(observer: IBindObserver<T>, trigger: Boolean): IContract = ObserverContract(observer)
-    fun bindTo( root: Bindable<T>) : IContract = BindContract(root)
-
-    private var externalTo: T = default
-    private var internalTo: T = default
-
-    private inner class BindContract( val externalBind: Bindable<T>) : IContract {
-        init {bind.field = externalBind.field}
-        val bindToWeakTrigger = externalBind.addWeakObserver { new, _ ->
-            if( internalTo != new) {
-                internalTo = new
-                bind.field = new
-            }
-        }
-        val weakToBindTrigger = bind.addWeakObserver { new, _ ->
-            if( externalTo != new) {
-                externalTo = new
-                externalBind.field = new
-            }
-        }
-        override fun void() {
-            bindToWeakTrigger.void()
-            weakToBindTrigger.void()
-        }
-    }
-    
-    private inner class ObserverContract(val observer: IBindObserver<T>) : IContract {
-        init {observers.add(observer)}
-        override fun void() {observers.remove(observer)}
-    }
-
-    private val binds = mutableListOf<BindContract>()
-    //private val triggers get() = observers.mapRemoveIfNull { it.triggers }
-    private val observers = mutableListOf<IBindObserver<T>>()
-
-    operator fun getValue(thisRef: Any, prop: KProperty<*>): T = bind.field
-    operator fun setValue(thisRef:Any, prop: KProperty<*>, value: T) {bind.field = value}
-}
-
-fun <T> Bindable<T>.bindWeaklyTo(root: Bindable<T>) : IContract
-{
-    this.field = root.field
-    val weakBind = WeakBindable(root.field)
-    return DoubleContract(
-            WeakObserverContract(weakBind.bindTo(root),root),
-            WeakObserverContract(weakBind.bindTo(this),this))
-}
-
-private class DoubleContract(contract1: IContract, contract2: IContract) : IContract
-{
-    var c1 : IContract? = contract1
-    var c2: IContract? = contract2
-    override fun void() {
-        c1?.void()
-        c2?.void()
-        c1 = null
-        c2 = null
-    }
-}
--- src/test/kotlin/sjunit/rbJvm/owl/WeakObserverTests.kt
+++ src/test/kotlin/sjunit/rbJvm/owl/WeakObserverTests.kt
@@ -1,5 +1,6 @@
 package sjunit.rbJvm.owl
 
+import rb.IContract
 import rb.owl.Observable
 import rbJvm.owl.WeakObserver
 import rbJvm.owl.addWeakObserver
@@ -36,5 +37,26 @@ class WeakObserverTests {
         obs.trigger { readback = it }
         println(readback)
         assertNull(readback)
+    }
+
+    @Test fun nofallThenFall() {
+        val obs = Observable<DataBlob>()
+        var weakContract1: IContract? = obs.addWeakObserver(DataBlob())
+        val weakContract2: IContract = obs.addWeakObserver(DataBlob())
+        System.gc()
+
+        // At this point neither Weak Obs should be dead because thy are both referenced at the root level as local variables
+        var numAlive = 0
+        obs.trigger { ++numAlive }
+        assertEquals(2, numAlive)
+
+        weakContract1 = null
+        weakContract2.void()
+        System.gc()
+
+        // at this point both should be dead
+        var numAlive2 = 0
+        obs.trigger { ++numAlive2 }
+        assertEquals(0, numAlive2)
     }
 }
